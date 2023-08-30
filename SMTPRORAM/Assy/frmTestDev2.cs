4e2db5c469c83c544a9736994b1a1511b015c2b5
xitrum2708 <hongfxhp@gmail.com>
2023-08-30 15:07:22 +0700

    test master

---
+++ SMTPRORAM/Assy/frmTestDev2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMTPRORAM.Assy
+{
+	public partial class frmTestDev2 : Form
+	{
+		public frmTestDev2()
+		{
+			InitializeComponent();
+		}
+	}
+}

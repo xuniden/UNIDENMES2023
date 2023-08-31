8644762be84ad40921b20791980e583ffe19f405
xitrum2708 <hongfxhp@gmail.com>
2023-08-31 09:30:25 +0700

    Tesst

--- UnidenDTO/UVModel.Designer.cs
+++ UnidenDTO/UVModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\000_Uniden\008_GitHub\SMTPRORAM-Combine\UnidenDTO\UVModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\001_C#Pro\000_GitHub\NewMES\UnidenDTO\UVModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
